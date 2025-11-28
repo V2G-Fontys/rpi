d8f80f7f1c6668fdf658a1dc9d80795cb6441316
Mike Weijts <99175828+MikeWeijts@users.noreply.github.com>
2025-11-28 13:08:11 +0100

    Added comments to digipot_controller
    Updates digipot_controller and added comments to make the code more clear

--- services/digipot_controller.py
+++ services/digipot_controller.py
@@ -4,6 +4,7 @@ from ws.status_ws import manager
 
 class DigipotService:
 	def __init__(self):
+		#Initialize spiDev
 		self.spi = None
 		self.spi = spidev.SpiDev()
 		self.spi.open(0, 0)
@@ -11,11 +12,19 @@ class DigipotService:
 		
 	async def set_digipot(self, value):
 		try:
+			#Changed Digipot register 0x00 via Spidev based on the value when calling the function
 			self.spi.xfer2([0x00, self.value])
+
+			#Print changes in the console
 			print(f"[DigiPot] Set digipot to value: {value}")
+
+			#Sent message to the websocket endpoint from the API
 			await manager.broadcast("/box/digipot", {"voltage": value, "error": False})
 		except Exception as e:
+			#Print changes in the console when something goes wrong
 			print(f"[DigiPot] Failed to set value: {e}")
+
+			#Sent message to the websocket endpoint from the API when a problem occurs
 			await manager.broadcast("/box/digipot", {"voltage": -1, "error": True})
 			
 	def stop(self):
