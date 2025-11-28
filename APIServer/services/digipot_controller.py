85a0acc85d250a89a959d74deddffeacd949a9a2
Mike Weijts <99175828+MikeWeijts@users.noreply.github.com>
2025-11-28 12:06:03 +0100

    Optimize SPI initialization and cleanup in DigipotService
    Refactor DigipotService to initialize SPI only once and close it in stop method.
    Improved structure and logic.

--- APIServer/services/digipot_controller.py
+++ APIServer/services/digipot_controller.py
@@ -4,24 +4,22 @@ from ws.status_ws import manager
 
 class DigipotService:
 	def __init__(self):
-		# self.logger = get_logger("DigipotService")  # Initialize logger
 		self.spi = None
+		self.spi = spidev.SpiDev()
+		self.spi.open(0, 0)
+		self.spi.max_speed_hz = 1000000
+		
 	async def set_digipot(self, value):
 		try:
-			self.value = value
-			self.spi = spidev.SpiDev()
-			self.spi.open(0, 0)
-			self.spi.max_speed_hz = 1000000
 			self.spi.xfer2([0x00, self.value])
 			print(f"[DigiPot] Set digipot to value: {value}")
 			await manager.broadcast("/box/digipot", {"voltage": value, "error": False})
 		except Exception as e:
 			print(f"[DigiPot] Failed to set value: {e}")
 			await manager.broadcast("/box/digipot", {"voltage": -1, "error": True})
-		finally:
-			if self.spi:
-				self.spi.close()
 			
 	def stop(self):
 		self._running = False
+		if self.spi:
+			self.spi.close()
 
