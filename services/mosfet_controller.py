28e9b50bfd25d27d226b872ead4fa734a52c9ad2
Mike Weijts <mikeweijts@gmail.com>
2025-12-03 15:51:16 +0100

    Added tests folder to test each service. Also added masfet_controller and testMosfetService

---
+++ services/mosfet_controller.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+from enum import Enum
+#from ws.status_ws import manager
+
+class State(Enum):
+	ON = 1
+	OFF = 0
+	
+class MosfetService:
+	def __init__(self):
+		GPIO.setmode(GPIO.BOARD)
+	async def set_mosfet(self, GPIOPin, State):
+		try:
+			GPIO.setup(GPIOPin, GPIO.OUT)
+			
+			if (State == 'ON'):
+				GPIO.output(GPIOPin, GPIO.HIGH)
+			elif (State == 'OFF'):
+				GPIO.output(GPIOPin, GPIO.LOW)
+
+			#Print changes in the console
+			print(f"[Mosfet] Set mosfet({GPIOPin}) to {State}")
+
+			#Send message to the websocket endpoint from the API
+			#await manager.broadcast("mosfet", {"GPIOPin": State, "error": False})
+		except Exception as e:
+			#Print changes in the console when something goes wrong
+			print(f"[Mosfet] Failed to set value: {e}")
+
+			#Send message to the websocket endpoint from the API when a problem occurs
+			#await manager.broadcast("mosfet", {"GPIOPin": -1, "error": True})
+			
+	def stop(self):
+		GPIO.cleanup()
