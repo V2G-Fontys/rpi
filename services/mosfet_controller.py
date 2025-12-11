66f10fadba9735b2438f46fc272baf39347af9d5
Mike Weijts <mikeweijts@gmail.com>
2025-12-11 20:11:59 +0100

    Updated mosfet_controller with boolean instead of enums, and disabled warning messages. In testMosfetService I added components with GPIO pin to make it more readable and updated the enum to boolean

--- services/mosfet_controller.py
+++ services/mosfet_controller.py
@@ -2,33 +2,32 @@ import RPi.GPIO as GPIO
 from enum import Enum
 #from ws.status_ws import manager
 
-class State(Enum):
-	ON = 1
-	OFF = 0
+State = bool
 	
 class MosfetService:
 	def __init__(self):
 		GPIO.setmode(GPIO.BOARD)
-	async def set_mosfet(self, GPIOPin, state: State):
+		GPIO.setwarnings(False)
+	async def set_mosfet(self, GPIOPin, State):
 		try:
 			GPIO.setup(GPIOPin, GPIO.OUT)
 			
-			if state == State.ON:
+			if State:
 				GPIO.output(GPIOPin, GPIO.HIGH)
-			elif state == State.OFF:
+			else:
 				GPIO.output(GPIOPin, GPIO.LOW)
 
 			#Print changes in the console
-			print(f"[Mosfet] Set mosfet({GPIOPin}) to {state.name}")
+			print(f"[Mosfet] Set mosfet({GPIOPin}) to {State}")
 
 			#Send message to the websocket endpoint from the API
-			#await manager.broadcast("mosfet", {"GPIOPin": GPIOPin, "State": State, "error": False})
+			#await manager.broadcast("mosfet", {"GPIOPin": State, "error": False})
 		except Exception as e:
 			#Print changes in the console when something goes wrong
 			print(f"[Mosfet] Failed to set value: {e}")
 
 			#Send message to the websocket endpoint from the API when a problem occurs
-			#await manager.broadcast("mosfet", {"GPIOPin": GPIOPin, "State": State, "error": True})
+			#await manager.broadcast("mosfet", {"GPIOPin": -1, "error": True})
 			
 	def stop(self):
 		GPIO.cleanup()
--- tests/testMosfetService.py
+++ tests/testMosfetService.py
@@ -5,6 +5,11 @@ import sys
 parent_dir = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(parent_dir))
 
+boostconverter = 11
+precharge = 33
+relayHV = 29
+relayLV = 31
+
 from services.mosfet_controller import MosfetService
 import asyncio
 
@@ -12,8 +17,11 @@ async def main():
     
     mosfet = MosfetService()
     
-    #Sets mosfet on or off set_mosfet({PIN}, '{ON/OFF}')
-    await mosfet.set_mosfet(11, 'OFF')
-    await mosfet.set_mosfet(13, 'ON')
+    #Sets mosfet on or off set_mosfet({PIN}, '{1/0}t')
+
+    await mosfet.set_mosfet(boostconverter, 0)
+    await mosfet.set_mosfet(precharge, 0)
+    await mosfet.set_mosfet(relayHV, 0)
+    await mosfet.set_mosfet(relayLV, 0)
 
 asyncio.run(main())
