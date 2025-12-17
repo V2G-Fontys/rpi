b8df60aa4d6c84c3eb207ecee2d8032927e8fba0
Mike Weijts <mikeweijts@gmail.com>
2025-12-17 12:41:30 +0100

    Updated mosfet_controller with boolean instead of enums, and disabled warning messages. In testMosfetService I added components with GPIO pin to make it more readable and updated the enum to boolean

--- services/mosfet_controller.py
+++ services/mosfet_controller.py
@@ -2,24 +2,23 @@ import RPi.GPIO as GPIO
 from enum import Enum
 #from ws.status_ws import manager
 
-class State(Enum):
-	ON = 1
-	OFF = 0
+State = bool
 	
 class MosfetService:
 	def __init__(self):
 		GPIO.setmode(GPIO.BOARD)
-	async def set_mosfet(self, GPIOPin, state: State):
+		GPIO.setwarnings(False)
+	async def set_mosfet(self, GPIOPin, State):
 		try:
 			GPIO.setup(GPIOPin, GPIO.OUT)
 			
-			if state == State.ON:
+			if State:
 				GPIO.output(GPIOPin, GPIO.HIGH)
-			elif state == State.OFF:
+			else:
 				GPIO.output(GPIOPin, GPIO.LOW)
 
 			#Print changes in the console
-			print(f"[Mosfet] Set mosfet({GPIOPin}) to {state.name}")
+			print(f"[Mosfet] Set mosfet({GPIOPin}) to {State}")
 
 			#Send message to the websocket endpoint from the API
 			#await manager.broadcast("mosfet", {"GPIOPin": GPIOPin, "State": State, "error": False})
